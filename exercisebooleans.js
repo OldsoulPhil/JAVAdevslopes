2a8c29ef695aeefe9c2bc99927174bbfbd890e58
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-08-29 09:43:25 -0400

    Created exercisebooleans.js file
    Just performed some boolean action with a if else statement.

---
+++ exercisebooleans.js
@@ -0,0 +1,17 @@
+//Consider some basic American traffic signaling. 
+ //1.  Create a string variable called currentLightColor
+ //2.  Set it's value to "Green", "Yellow", or "Red"
+ //3.  Create conditional if / else logic to print: "The light is X" where X is the value of the light variable
+ //4.  The if /else clause should print the appropriate color
+
+
+// Type in the traffic lights color here
+var currentLightColor = 'Red';
+// perform an if else statement to have the correct color
+if (currentLightColor == 'Green') {
+  console.log('The light is' + ' ' + currentLightColor)
+} else if (currentLightColor == 'Yellow') {
+  console.log('The light is' + ' ' + currentLightColor)
+} else {
+  console.log('The light is' + ' ' + currentLightColor)
+};
