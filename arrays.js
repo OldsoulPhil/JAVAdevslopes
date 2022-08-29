9d3e466a1a0ea9ca8c67514a925a8bcb8b9b42c5
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-08-29 12:51:35 -0400

    Created arrays.js file
    everything about arrays and its basics for JavaScript.

---
+++ arrays.js
@@ -0,0 +1,26 @@
+// JavaScript Arrays
+// Colleciton of data
+// usually want to make the array plural 
+var tvShows = [
+  'The Walking Dead', 
+  'Breaking Bad', 
+  'The Office'
+];
+
+console.log(tvShows[0]);
+
+// initiated a blank array
+var userSavedMovies = [];
+
+// push method pushes values to the end of the array
+userSaveMovies.push('Never Ending Story');
+userSaveMovies.push('The Matrix');
+userSaveMovies.push('Braveheart');
+
+// pop will remove the last value of the array list
+userSavedMovies.pop();
+
+console.log(userSaveMovies);
+
+//array has indexing properties which can access the value
+//objects you have named properties
