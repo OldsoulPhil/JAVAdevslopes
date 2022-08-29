f882148e2634233c32a40407afd18797a7bed82b
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-08-28 21:23:03 -0400

    Created variables.js file
    These are what variables are in JavaScript.

---
+++ variables.js
@@ -0,0 +1,29 @@
+
+// variable stores a value
+// var reserved Keyword(s), which are tokens that look like identifiers but have special meanings in JavaScript.
+// identifiers are used to link a value with a name.
+// userName is the name of the variable
+// = assignment operator
+// on the right you assign it a value in this case a string
+// create variables to store value(data) for a temporary time in temporary memory(RAM-random access memory).
+// var - prior ES2015 / ES6
+var userName = 'wasssup';
+// these are numaric values
+var totalWins = 30;
+var totalLosses = 10;
+
+                /* Variable Rules */
+
+//variables should have a descriptive variable name
+//cannot contain a hyphen (-) or space
+//cannot start a variable with a number
+//cannot use a reserved word. var for example, let, if, true/false
+//variables are case sensative
+
+
+// let, const = ES2015 / ES6
+
+let name = 'James';
+const age = 25;
+
+console.log(userName);
