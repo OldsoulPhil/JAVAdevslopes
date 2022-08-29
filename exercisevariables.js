e457a2f7ee6dca7d2322b862a2ea19945a459bf3
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-08-28 22:03:51 -0400

    Created exercisevariables.js file
    Created attributes and properties of a car using variable types (i.e. stings, numbers, booleans)

---
+++ exercisevariables.js
@@ -0,0 +1,28 @@
+//Create variables for each of the following attributes of a car and assign it a value.
+
+//Example: var hasPowerWindows = true;
+
+//1.  make
+
+//2.  model
+
+//3.  year
+
+//4. color
+
+//5.  isManualTransmission
+
+//6. numSeats
+
+//7. mileage
+
+var make = 'Subaru';
+var model = 'Outback';
+var year = '2015';
+var color = 'Red';
+var inManualTransmission = false;
+var numSeats = 5;
+var mileage = 78,000;
+
+console.log(model);
+console.log(mileage);
