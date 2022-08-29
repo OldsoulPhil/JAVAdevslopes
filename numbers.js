8f9d85ab48723f20a25340e754294c36d0425704
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-08-28 23:33:36 -0400

    Created numbers.js file
    Everything about variables using numbers as the values.

---
+++ numbers.js
@@ -0,0 +1,87 @@
+// JavaScript Numbers
+
+// intager just a whole number
+var totalUsers = 42;
+
+// float number because of the .
+var purchase = 46.00;
+
+// scientific notation
+// e5 adds five 0
+var sciNot = 123e5;
+
+// e-5 adds five 0 after . decimal points
+var otherSciNot = 123e-5;
+
+var numVal = 5 + 5;
+
+// 5+5, 5-5, 5/5, 5*5
+// remainder operator is the %
+
+// Java is only accurate up to 15 numbers
+// over 15 turns the number into sceintific notation
+
+// when working with decimals you can have 17 numbers
+// sometimes arithmetic on floating point numbers is not always 100% accurate
+var numValone = 0.1 + 0.2;
+
+// uses oder of operations
+// PENDAS
+// parenthasies, exponent, Multiplication *, Division /, Addition +, Subtraction -
+
+// numeric string
+// adds(concatinates) the number and number(number as text) together which = 105 years
+// without the years it would = 105
+//anything other than +
+var numberVal = 10 + '5 years';
+
+// toString() method
+// toString() method returns a number as a string
+let x = 123;
+x.toString();
+(123).toString();
+(100 + 23).toString();
+
+// toExponential() method
+// toExponential() returns a string, with a number rounded and written using exponential notation
+let x = 9.656;
+x.toExponential(2);
+x.toExponential(4);
+x.toExponential(6);
+
+// toFixed() method
+// toFixed() returns a string, with the number written with a specified number of decimals
+let x = 9.656;
+x.toFixed(0);
+x.toFixed(2):
+x.toFixed(4):
+x.toFixed(6):
+
+// toPrecision() method
+// toPrecision() returns a string, with a number written with a specified length
+let x = 9.656;
+x.toPrecision();
+x.toPrecision(2);
+x.toPrecision(4);
+x.toPrecision(6);
+
+// valueOf() method
+// valueOf() returns a number as a number
+let x = 123;
+x.valueOf();
+(123).valueOf();
+(100 + 23).valueOf();
+
+/* There are 3 JavaScript methods that can be used to convert variables to numbers:
+
+The Number() method Returns a number, converted from its argument.
+The parseInt() method Parses its argument and returns a floating point number
+The parseFloat() method Parses its argument and returns an integer
+
+These methods are not number methods, but global JavaScript methods. */
+
+
+
+
+
+
